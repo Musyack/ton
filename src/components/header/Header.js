68038ef0d455df26ed3cbc0162bb4702baeaf6f3
fushy <fushingggslave@gmail.com>
2023-01-22 20:49:40 +0300

    2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,13 @@ import {useTelegram} from "../../hooks/useTelegram";
 
 const Header = () => {
     const {user} = useTelegram()
+    console.log(user.username)
     return (
         <>
             <header className="header">
                 <div className="header__logo"></div>
 
-                <div className="header__wallet">{user.username}</div>
+                <div className="header__wallet">ssss</div>
             </header>
         </>
     );
